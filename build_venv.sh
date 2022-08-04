2e63f5602c9ec3549f75d84330a45a393fad3533
Ivo Tzvetkov <ivotkv@gmail.com>
2022-08-04 15:00:25 +1000

    basic venv & config

---
+++ build_venv.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+set -x
+
+rm -rf venv
+python3 -m venv venv
+source venv/bin/activate
+
+pip install --upgrade pip
+pip install --upgrade setuptools
+
+pip install ipython
+
+pip install -r requirements.txt
---
+++ config.py
@@ -0,0 +1,8 @@
+import yaml
+from os.path import abspath, dirname, join
+
+WORKING_DIR = dirname(abspath(__file__))
+CONFIG_FILE = join(WORKING_DIR, 'config.yaml')
+
+with open(CONFIG_FILE, 'r') as f:
+    config = yaml.safe_load(f.read())
